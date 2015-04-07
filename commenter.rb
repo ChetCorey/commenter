9d7f1d97133bc3d4b5c729aeadc2f4e29d402c72
Chet Corey <chetcorey@hotmail.com>
2015-04-07 17:40:43 -0400

    db up and running

--- commenter.rb
+++ commenter.rb
@@ -1,21 +1,42 @@
 require 'sinatra'
+require 'data_mapper'
 
-get '/hello/:name' do
-  "Hello there, #{params[:name]}."
-end
-get '/' do
-  "Sup, World!"
-end
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/commenter.db")
 
-get '/about' do
-  'A little about me.'
-end
-
-get '/form' do
-  erb :form
+class Note
+include DataMapper::Resource
+  property :id, Serial
+  property :content, Text, :required => true
+  property :complete, Boolean, :required => true, :default => false
+  property :created_at, DateTime
+  property :updated_at, DateTime
 end
 
+DataMapper.finalize.auto_upgrade!
 
-post '/form' do
-  "You said '#{params[:message]}'"
-end
+# get '/' do
+#   @notes = Note.all :order => :id.desc
+#   @title = 'All Notes'
+#   erb :home
+# end
+
+
+# get '/hello/:name' do
+#   "Hello there, #{params[:name]}."
+# end
+# get '/' do
+#   "Sup, World!"
+# end
+#
+# get '/about' do
+#   'A little about me.'
+# end
+#
+# get '/form' do
+#   erb :form
+# end
+#
+#
+# post '/form' do
+#   "You said '#{params[:message]}'"
+# end
